2fd21811e327683c2eb990739419428f80cb6ea1
Dnyaneshwar <dnyaneshrt>
2021-08-31 10:48:42 +0530

    back button issue resolved

--- app/src/main/java/com/tech/webviewtest/MainActivity.kt
+++ app/src/main/java/com/tech/webviewtest/MainActivity.kt
@@ -1,13 +1,16 @@
 package com.tech.webviewtest
 
+import android.content.DialogInterface
 import android.graphics.Bitmap
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.webkit.WebResourceRequest
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import android.widget.*
+import androidx.appcompat.app.AlertDialog
 
 class MainActivity : AppCompatActivity() {
 
@@ -18,7 +21,7 @@ class MainActivity : AppCompatActivity() {
     var btn_java: ImageButton? = null
     var btn_insta: ImageButton? = null
     var btn_github: ImageButton? = null
-    var progress_bar:ProgressBar?=null
+    var progress_bar: ProgressBar? = null
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -35,13 +38,13 @@ class MainActivity : AppCompatActivity() {
             override fun onPageStarted(view: WebView?, url: String?, favicon: Bitmap?) {
                 super.onPageStarted(view, url, favicon)
 
-                Toast.makeText(this@MainActivity,url,Toast.LENGTH_SHORT).show()
-                progress_bar?.visibility=View.VISIBLE
+//                Toast.makeText(this@MainActivity, url, Toast.LENGTH_SHORT).show()
+                progress_bar?.visibility = View.VISIBLE
             }
 
             override fun onPageFinished(view: WebView?, url: String?) {
                 super.onPageFinished(view, url)
-                progress_bar?.visibility=View.GONE
+                progress_bar?.visibility = View.GONE
             }
 
 
@@ -60,7 +63,7 @@ class MainActivity : AppCompatActivity() {
         btn_java = findViewById(R.id.btn_javatpoint)
         btn_insta = findViewById(R.id.btn_instagram)
         btn_github = findViewById(R.id.btn_github)
-        progress_bar=findViewById(R.id.progress_bar)
+        progress_bar = findViewById(R.id.progress_bar)
 
     }
 
@@ -91,5 +94,41 @@ class MainActivity : AppCompatActivity() {
         }
 
 
+    }
+
+    //this method will execute after clicking on back button.
+    //Alert dialog
+    @Override
+    override fun onBackPressed() {
+
+        if (webView!!.canGoBack()) {
+            webView!!.goBack()
+        } else {
+            var alertDialog = AlertDialog.Builder(this)
+            alertDialog.setTitle("Exit App!")
+            alertDialog.setMessage("Do you really want to exit app?")
+            alertDialog.setIcon(R.drawable.ic_baseline_exit_to_app_24)
+            alertDialog.setCancelable(false)
+
+            var listener = DialogInterface.OnClickListener { dialogInterface, which ->
+                if (which == DialogInterface.BUTTON_POSITIVE) {
+                    super.onBackPressed()
+                    finish()
+                } else if (which == DialogInterface.BUTTON_NEGATIVE) {
+                    dialogInterface.dismiss()
+
+                } else if (which == DialogInterface.BUTTON_NEUTRAL) {
+                    dialogInterface.dismiss()
+                }
+
+            }
+
+            alertDialog.setPositiveButton("yes", listener)
+            alertDialog.setNegativeButton("no", listener)
+            alertDialog.setNeutralButton("cancel", listener)
+            alertDialog.show()
+
+
+        }
     }
 }
--- app/src/main/java/com/tech/webviewtest/SplashActivity.kt
+++ app/src/main/java/com/tech/webviewtest/SplashActivity.kt
@@ -4,18 +4,27 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import com.airbnb.lottie.LottieAnimationView
 
 class SplashActivity : AppCompatActivity() {
+    var anim_view: LottieAnimationView? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
 
+        anim_view = findViewById(R.id.animation_view)
+
+//        anim_view?.setAnimation(R.raw.education)
+        anim_view?.animate()
+
 
         Handler().postDelayed({
-            //this code will ececute after 3 seconds
+            //this code will execute after 3 seconds
             startActivity(Intent(this, MainActivity::class.java))
+            finish()
 
-        }, 4000)
+        }, 5500)
     }
 }
 
